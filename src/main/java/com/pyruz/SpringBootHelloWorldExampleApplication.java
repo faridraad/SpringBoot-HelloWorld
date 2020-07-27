a11e96e55783aa73b261544bf99a676e5f6d512f
Pyruz <Pyruz.Janbaaz@gmail.com>
2020-07-27 13:29:31 +0430

    first commit in gitHub!

---
+++ src/main/java/com/pyruz/SpringBootHelloWorldExampleApplication.java
@@ -0,0 +1,11 @@
+package com.pyruz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHelloWorldExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
+    }
+}
